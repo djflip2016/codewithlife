106b6edc91d2d56854d8522fac3e324b35937a2c
Daniel Filipkowski <df07220131@gmail.com>
2017-06-20 00:25:22 -0500

    initial commit

---
+++ interact.js
@@ -0,0 +1,8 @@
+console.log("begin program");
+var message = prompt("welcome. What is your name?");
+var how = prompt("How did you hear about me?");
+console.log(message);
+console.log(how);
+message+= message;
+message+= "welcome to the site";
+console.log("end program");
