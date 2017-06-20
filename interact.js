dc24cb63ea821a464ddbfbd7fc930e2d5f22fbe1
Daniel Filipkowski <df07220131@gmail.com>
2017-06-20 00:34:46 -0500

    removed script

--- interact.js
+++
@@ -1,8 +0,0 @@
-console.log("begin program");
-var message = prompt("welcome. What is your name?");
-var how = prompt("How did you hear about me?");
-console.log(message);
-console.log(how);
-message+= message;
-message+= "welcome to the site";
-console.log("end program");
